77b3589f8ca6f63cbde2d3825524ccf309cb1219
mactub2004-debug <mactub2004@gmail.com>
2025-10-26 16:35:59 -0300

    Toda bosta duplicada AFURA
    eso

--- page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/page.tsx
-export default function HomePage() {
-  // Esta página DEBE existir para que Next.js resuelva la ruta '/'.
-  // El layout.tsx redirigirá al usuario antes de que esto se muestre.
-  return null;
-}
