d87981528889ef5f2717ba945cc12505fd4e1fc6
mactub2004-debug <mactub2004@gmail.com>
2025-10-26 17:03:47 -0300

    Update calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,12 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        IconLeft: ({ ...props_ }) => <ChevronLeft className="h-4 w-4" {...props_} />, // Corrección aplicada
+        IconRight: ({ ...props_ }) => <ChevronRight className="h-4 w-4" {...props_} />, // Corrección aplicada
       }}
       {...props}
     />
